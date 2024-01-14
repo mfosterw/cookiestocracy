f94ba081ab476950ca2c3b3ff8efda15beab482c
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2024-01-14 13:23:30 -0700

    Improve template test coverage

--- democrasite/webiscite/tests/test_templates.py
+++ democrasite/webiscite/tests/test_templates.py
@@ -7,8 +7,8 @@ from ..models import Bill
 from .factories import BillFactory
 
 
-class TestWebisciteTemplates:
-    def test_bill_detail_open(self, bill: Bill, client: Client):
+class TestBillDetailTemplate:
+    def test_bill_open(self, bill: Bill, client: Client):
         response = client.get(reverse("webiscite:bill-detail", args=(bill.id,)))
 
         assert b"Log in to vote" in response.content
@@ -21,7 +21,7 @@ class TestWebisciteTemplates:
         assert b"svg" in response.content
         assert bill.get_state_display().encode() not in response.content
 
-    def test_bill_detail_closed(self, client: Client, user):
+    def test_bill_closed(self, client: Client, user):
         bill = BillFactory(state=Bill.CLOSED, author=user)
 
         response = client.get(reverse("webiscite:bill-detail", args=(bill.id,)))
@@ -33,7 +33,7 @@ class TestWebisciteTemplates:
         assert b"svg" not in response.content
         assert bill.get_state_display().encode() in response.content
 
-    def test_bill_detail_approved(self, client: Client, user):
+    def test_bill_approved(self, client: Client, user):
         bill = BillFactory(state=Bill.APPROVED, author=user, constitutional=True)
 
         response = client.get(reverse("webiscite:bill-detail", args=(bill.id,)))
@@ -46,6 +46,8 @@ class TestWebisciteTemplates:
         assert bill.get_state_display().encode() in response.content
         assert b"Constitution" in response.content
 
+
+class TestBillUpdateTemplate:
     def test_bill_form(self, bill: Bill, client: Client):
         client.force_login(bill.author)
 
@@ -54,7 +56,9 @@ class TestWebisciteTemplates:
         assert response.status_code == 200
         assert response.templates[0].name == "webiscite/bill_form.html"
 
-    def test_bill_list_empty(self, client: Client):
+
+class TestBillListTemplate:
+    def test_empty(self, client: Client):
         response = client.get(reverse("webiscite:index"))
 
         assert response.status_code == 200
@@ -63,7 +67,7 @@ class TestWebisciteTemplates:
         assert b"you haven't proposed any bills" not in response.content
         assert b"you haven't voted on any bills" not in response.content
 
-    def test_bill_list_logged_out(self, bill: Bill, client: Client):
+    def test_logged_out(self, bill: Bill, client: Client):
         response = client.get(reverse("webiscite:index"))
 
         assert response.status_code == 200
@@ -71,7 +75,16 @@ class TestWebisciteTemplates:
         assert b"Log in to vote" in response.content
         assert b"vote.js" not in response.content
 
-    def test_bill_list_logged_in(self, bill: Bill, client: Client, user):
+    def test_amendment(self, client: Client):
+        bill = BillFactory(constitutional=True)
+
+        response = client.get(reverse("webiscite:index"))
+
+        assert response.status_code == 200
+        assert bill.name.encode() in response.content
+        assert b"Constitution" in response.content
+
+    def test_logged_in(self, bill: Bill, client: Client, user):
         client.force_login(user)
 
         response = client.get(reverse("webiscite:index"))
@@ -90,7 +103,18 @@ class TestWebisciteTemplates:
         assert b"you haven't proposed any bills" in response.content
         assert b"you haven't voted on any bills" not in response.content
 
-    def test_my_bills_populated(self, bill: Bill, client: Client):
+    def test_my_bills_approved(self, client: Client):
+        bill = BillFactory(state=Bill.APPROVED)
+        client.force_login(bill.author)
+
+        response = client.get(reverse("webiscite:my-bills"))
+
+        assert response.status_code == 200
+        assert b"Log in to vote" not in response.content
+        assert b"vote.js" in response.content
+
+    def test_my_bills_failed(self, client: Client):
+        bill = BillFactory(state=Bill.FAILED)
         client.force_login(bill.author)
 
         response = client.get(reverse("webiscite:my-bills"))
