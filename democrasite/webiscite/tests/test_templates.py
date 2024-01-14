9af1de0bf9cb40ae34c4ab2b5904d9d1753cb52c
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2024-01-13 20:00:19 -0700

    Set factory test defaults

--- democrasite/webiscite/tests/test_templates.py
+++ democrasite/webiscite/tests/test_templates.py
@@ -8,8 +8,21 @@ from .factories import BillFactory
 
 
 class TestWebisciteTemplates:
-    def test_bill_detail(self, client: Client, user):
-        bill = BillFactory(state=Bill.CLOSED, author=user, constitutional=True)
+    def test_bill_detail_open(self, bill: Bill, client: Client):
+        response = client.get(reverse("webiscite:bill-detail", args=(bill.id,)))
+
+        assert b"Log in to vote" in response.content
+
+        client.force_login(bill.author)
+
+        response = client.get(reverse("webiscite:bill-detail", args=(bill.id,)))
+
+        assert b"vote.js" in response.content
+        assert b"svg" in response.content
+        assert bill.get_state_display().encode() not in response.content
+
+    def test_bill_detail_closed(self, client: Client, user):
+        bill = BillFactory(state=Bill.CLOSED, author=user)
 
         response = client.get(reverse("webiscite:bill-detail", args=(bill.id,)))
 
@@ -20,31 +33,28 @@ class TestWebisciteTemplates:
         assert b"svg" not in response.content
         assert bill.get_state_display().encode() in response.content
 
-        bill.state = Bill.OPEN
-        bill.save()
+    def test_bill_detail_approved(self, client: Client, user):
+        bill = BillFactory(state=Bill.APPROVED, author=user, constitutional=True)
 
         response = client.get(reverse("webiscite:bill-detail", args=(bill.id,)))
 
-        assert b"Log in to vote" in response.content
+        assert response.status_code == 200
+        assert response.templates[0].name == "webiscite/bill_detail.html"
+        assert b"Log in to vote" not in response.content
+        assert b"vote.js" not in response.content
+        assert b"svg" not in response.content
+        assert bill.get_state_display().encode() in response.content
+        assert b"Constitution" in response.content
 
-        client.force_login(user)
-
-        response = client.get(reverse("webiscite:bill-detail", args=(bill.id,)))
-
-        assert b"vote.js" in response.content
-        assert b"svg" in response.content
-        assert bill.get_state_display().encode() not in response.content
-
-    def test_bill_form(self, client: Client, user):
-        bill = BillFactory(author=user)
-        client.force_login(user)
+    def test_bill_form(self, bill: Bill, client: Client):
+        client.force_login(bill.author)
 
         response = client.get(reverse("webiscite:bill-update", args=(bill.id,)))
 
         assert response.status_code == 200
         assert response.templates[0].name == "webiscite/bill_form.html"
 
-    def test_bill_list_empty(self, client: Client, user):
+    def test_bill_list_empty(self, client: Client):
         response = client.get(reverse("webiscite:index"))
 
         assert response.status_code == 200
@@ -53,6 +63,25 @@ class TestWebisciteTemplates:
         assert b"you haven't proposed any bills" not in response.content
         assert b"you haven't voted on any bills" not in response.content
 
+    def test_bill_list_logged_out(self, bill: Bill, client: Client):
+        response = client.get(reverse("webiscite:index"))
+
+        assert response.status_code == 200
+        assert bill.name.encode() in response.content
+        assert b"Log in to vote" in response.content
+        assert b"vote.js" not in response.content
+
+    def test_bill_list_logged_in(self, bill: Bill, client: Client, user):
+        client.force_login(user)
+
+        response = client.get(reverse("webiscite:index"))
+
+        assert response.status_code == 200
+        assert bill.name.encode() in response.content
+        assert b"Log in to vote" not in response.content
+        assert b"vote.js" in response.content
+
+    def test_my_bills_empty(self, client: Client, user):
         client.force_login(user)
 
         response = client.get(reverse("webiscite:my-bills"))
@@ -61,35 +90,31 @@ class TestWebisciteTemplates:
         assert b"you haven't proposed any bills" in response.content
         assert b"you haven't voted on any bills" not in response.content
 
+    def test_my_bills_populated(self, bill: Bill, client: Client):
+        client.force_login(bill.author)
+
+        response = client.get(reverse("webiscite:my-bills"))
+
+        assert response.status_code == 200
+        assert b"Log in to vote" not in response.content
+        assert b"vote.js" in response.content
+
+    def test_my_bill_votes_empty(self, client: Client, user):
+        client.force_login(user)
+
         response = client.get(reverse("webiscite:my-bill-votes"))
 
         assert b"No bills" not in response.content
         assert b"you haven't proposed any bills" not in response.content
         assert b"you haven't voted on any bills" in response.content
 
-    def test_bill_list_populated(self, client: Client, user):
-        bill = BillFactory(state=Bill.OPEN, author=user, constitutional=True)
+    def test_my_bill_votes_populated(self, bill: Bill, client: Client, user):
+        bill.vote(True, user)
+        client.force_login(user)
 
-        response = client.get(reverse("webiscite:index"))
+        response = client.get(reverse("webiscite:my-bill-votes"))
 
         assert response.status_code == 200
         assert bill.name.encode() in response.content
-        assert bill.get_state_display().encode() not in response.content
-        assert b"Log in to vote" in response.content
-        assert b"vote.js" not in response.content
-
-        client.force_login(user)
-
-        response = client.get(reverse("webiscite:index"))
-
-        assert b"Log in to vote" not in response.content
-
-        bill.state = Bill.CLOSED
-        bill.save()
-
-        response = client.get(reverse("webiscite:my-bills"))
-
-        assert bill.name.encode() in response.content
-        assert bill.get_state_display().encode() in response.content
         assert b"Log in to vote" not in response.content
         assert b"vote.js" in response.content
