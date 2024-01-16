bf95363288a384ebb7978cf12e1b4f98f55c0fe6
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2024-01-16 15:14:30 -0700

    improve coverage

--- democrasite/webiscite/admin.py
+++ democrasite/webiscite/admin.py
@@ -1,14 +1,7 @@
 """This module registers Webiscite models for usage on the Django admin site"""
-from django.conf import settings
 from django.contrib import admin
-from django.contrib.auth.decorators import login_required
 
 from .models import Bill
-
-if settings.DJANGO_ADMIN_FORCE_ALLAUTH:
-    # Force the `admin` sign in process to go through the `django-allauth` workflow:
-    # https://django-allauth.readthedocs.io/en/stable/advanced.html#admin
-    admin.site.login = login_required(admin.site.login)  # type: ignore[method-assign]
 
 # Register your models here.
 admin.site.register(Bill)
