5dc7aade4d0fa99da3a54da74601371da8b72764
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2024-02-29 01:41:22 -0500

    autogenerated frontend api (#68)

---
+++ democrasite-frontend/api/auto/models/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthToken';
+export * from './Bill';
+export * from './PatchedBill';
+export * from './PatchedUser';
+export * from './PullRequest';
+export * from './User';
