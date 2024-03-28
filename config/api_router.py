af881186019c20c0e726aa05db5fcbf901b2d176
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2024-03-28 19:28:50 -0400

    Remove unused auth api routes

--- config/api_router.py
+++ config/api_router.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.urls import include
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
@@ -13,11 +12,10 @@ router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 router.register("users", UserViewSet)
 router.register("bills", BillViewSet)
 
-# Unfortunately if we want automatical links for models we can't use a namespace
+# Unfortunately if we want automatic links for models we can't use a namespace
 # but I may reconsider anyway
 # app_name = "api"  # noqa: ERA001
 urlpatterns = [
     *router.urls,
-    path("auth/", include("dj_rest_auth.urls")),
     path("auth/github/", GitHubLogin.as_view(), name="github_login"),
 ]
