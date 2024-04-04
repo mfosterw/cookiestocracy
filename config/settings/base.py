ff23ddcf5c4014445049a9d8725fd79375837d48
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2024-04-04 00:25:00 -0400

    fix mypy outside container

--- config/settings/base.py
+++ config/settings/base.py
@@ -44,7 +44,7 @@ LOCALE_PATHS = [str(BASE_DIR / "locale")]
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-DATABASES = {"default": env.db("DATABASE_URL")}
+DATABASES = {"default": env.db("DATABASE_URL", default="postgres:///democrasite")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
