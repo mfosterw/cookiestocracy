3c768f660d784be65957c08adf4f2336c8641808
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2024-04-18 13:24:28 -0400

    Vote button reducer (#117)
    * Moved sessionProvider to Bill
    
    * Refactor VoteButtons  to use useReducer instead of useState

--- democrasite-frontend/components/BillList.tsx
+++ democrasite-frontend/components/BillList.tsx
@@ -1,14 +1,6 @@
-"use client";
-
-// TODO: This has to be a client component because it uses SessionProvider
-// This could be avoided by using getServerSession in Bill.tsx but that would
-// require making Bill async, which would also force this to be a client component
-// due to Mantine. It may be worth it to ugrapde to Auth.js v5 before it leaves beta
-// to make sessions more convenient.
 import { Card, Grid, GridCol } from "@mantine/core";
 import { Bill } from "@/components";
 import type { Bill as BillType } from "@/lib/models";
-import { SessionProvider } from "next-auth/react";
 
 export function BillList({ bill_list }: { bill_list: BillType[] }) {
   const cards = bill_list.map((bill: BillType) => (
@@ -19,9 +11,5 @@ export function BillList({ bill_list }: { bill_list: BillType[] }) {
     </GridCol>
   ));
 
-  return (
-    <SessionProvider>
-      <Grid gutter={{ base: 5, xs: "md", xl: "xl" }}>{cards}</Grid>
-    </SessionProvider>
-  );
+  return <Grid gutter={{ base: 5, xs: "md", xl: "xl" }}>{cards}</Grid>;
 }
