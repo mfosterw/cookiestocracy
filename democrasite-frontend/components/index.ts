65d81ed737f717e9779973c2f5f51df69d5dc1f0
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2025-02-09 22:16:59 +0000

    Remove detached frontend

--- democrasite-frontend/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./Auth";
-export * from "./Bill";
-export * from "./BillList";
-export * from "./VoteButtons";
--- democrasite-frontend/lib/actions.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { billsApi } from "./api";
-import { BillsVoteCreateRequest } from "./auto";
-
-// Server actions to call on client side
-export const billsVote = async (params: BillsVoteCreateRequest) =>
-  await billsApi.billsVoteCreate(params);
