5b9369f24085039c6791b1c70964393b789e5986
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2025-02-10 00:38:27 +0000

    Remove API schema

--- generate-api-client.sh
+++
@@ -1,5 +0,0 @@
-python manage.py spectacular --format openapi-json --file schema.json
-cd democrasite-frontend
-rm -r lib/auto
-openapi-generator-cli generate -g typescript-fetch -i ../schema.json -o lib/auto
-pre-commit run --all-files
