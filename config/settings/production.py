45e27bc98bbac0057b92d4e41f9614328d6928e4
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2025-08-31 12:26:32 -0500

    fix deployment

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,7 +9,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["democrasite.herokuapp.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["democrasite.dev"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -72,7 +72,7 @@ STORAGES = {
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="Democrasite <noreply@democrasite.tech>",
+    default="Democrasite <noreply@democrasite.dev>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
