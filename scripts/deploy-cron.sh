328b673376ceeeca4a422f26d1ffa9396f386c00
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2025-09-03 19:37:23 -0600

    Prevent build from being created in server repository

--- scripts/deploy-cron.sh
+++ scripts/deploy-cron.sh
@@ -3,6 +3,5 @@
 # Scripts adapted from https://www.youtube.com/watch?v=jFrGhodqC08 by Tom Delande
 # Licensed under https://creativecommons.org/licenses/by/4.0/
 
-LOCK_FILE="$(pwd)/build.lock"
 cd /home/matthew/democrasite
-flock -n $LOCK_FILE scripts/deploy.sh >> build.log 2>&1
+flock -n "~/build.lock" scripts/deploy.sh >> ~/build.log 2>&1
