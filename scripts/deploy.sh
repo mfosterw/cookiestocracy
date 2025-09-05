4fa93e32a98bf217c6b580bb3c6b2e05407d9593
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2025-09-04 18:45:46 -0600

    Apparently I didn't test right :/ (#240)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,17 +21,17 @@ elif [ $LOCAL = $BASE ]; then
 
     COMPOSE_FILE="/home/matthew/democrasite/docker-compose.production.yml"
     echo "$(date --utc +%FT%TZ): Running build..."
-    docker compose $COMPOSE_FILE rm -f
-    docker compose $COMPOSE_FILE build
+    docker compose -f $COMPOSE_FILE rm -f
+    docker compose -f $COMPOSE_FILE build
 
     OLD_CONTAINER=$(docker ps -aqf "name=django")
     echo "$(date --utc +%FT%TZ): Scaling server up..."
-    BUILD_VERSION=$BUILD_VERSION docker compose $COMPOSE_FILE up -d --no-deps --scale django=2 --no-recreate django
+    BUILD_VERSION=$BUILD_VERSION docker compose -f $COMPOSE_FILE up -d --no-deps --scale django=2 --no-recreate django
 
     sleep 30
 
     echo "$(date --utc +%FT%TZ): Scaling old server down..." docker container rm -f $OLD_CONTAINER
-    docker compose $COMPOSE_FILE up -d --no-deps --scale django=1 --no-recreate django
+    docker compose -f $COMPOSE_FILE up -d --no-deps --scale django=1 --no-recreate django
 elif [ $REMOTE = $BASE ]; then
      echo "$(date --utc +%FT%TZ): Local changes detected, you may need to stash"
 else
