fc55fb74c79196fa4248e3e05be3237dc17d51a6
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2025-09-07 21:21:50 -0600

    Add history to loaddata and admin

--- democrasite/activitypub/admin.py
+++ democrasite/activitypub/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 
 from democrasite.activitypub.models import Note
 from democrasite.activitypub.models import Person
 
 # Register your models here.
-admin.site.register(Person)
-admin.site.register(Note)
+admin.site.register(Person, SimpleHistoryAdmin)
+admin.site.register(Note, SimpleHistoryAdmin)
--- democrasite/webiscite/admin.py
+++ democrasite/webiscite/admin.py
@@ -1,10 +1,11 @@
 """This module registers Webiscite models for usage on the Django admin site"""
 
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 
 from .models import Bill
 from .models import PullRequest
 
 # Register your models here.
-admin.site.register(Bill)
-admin.site.register(PullRequest)
+admin.site.register(Bill, SimpleHistoryAdmin)
+admin.site.register(PullRequest, SimpleHistoryAdmin)
