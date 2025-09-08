1aa8665f2f1b82438675e96a6ed454057f06821a
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2025-09-08 17:48:20 -0600

    Improve note admin

--- democrasite/activitypub/admin.py
+++ democrasite/activitypub/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 from simple_history.admin import SimpleHistoryAdmin
 
 from democrasite.activitypub.models import Note
 from democrasite.activitypub.models import Person
 
-# Register your models here.
+
+@admin.register(Note)
+class HistoryMpttAdmin(SimpleHistoryAdmin, MPTTModelAdmin):
+    pass
+
+
 admin.site.register(Person, SimpleHistoryAdmin)
-admin.site.register(Note, SimpleHistoryAdmin)
