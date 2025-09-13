86db3571cf9c4942b75edb723db82b16758bb818
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2025-09-13 07:57:18 -0600

    minor deployment fixes (#256)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,37 +3,21 @@
 # Scripts adapted from https://www.youtube.com/watch?v=jFrGhodqC08 by Tom Delande
 # Licensed under https://creativecommons.org/licenses/by/4.0/
 
-echo "$(date --utc +%FT%TZ): Fetching remote repository..."
-git fetch
 
-UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
+BUILD_VERSION=$(git rev-parse HEAD)
+echo "#(date --utc +%FT%TZ): Releasing new server version. $BUILD_VERSION"
 
-if [ $LOCAL = $REMOTE ]; then
-    echo "$(date --utc +%FT%TZ): No changes detected in git"
-elif [ $LOCAL = $BASE ]; then
-    git pull
+COMPOSE_FILE="/home/matthew/democrasite/docker-compose.production.yml"
+echo "$(date --utc +%FT%TZ): Running build..."
+docker compose -f $COMPOSE_FILE rm -f
+docker compose -f $COMPOSE_FILE build
 
-    BUILD_VERSION=$(git rev-parse HEAD)
-    echo "#(date --utc +%FT%TZ): Releasing new server version. $BUILD_VERSION"
+OLD_CONTAINER=$(docker ps -aqf "name=django")
+echo "$(date --utc +%FT%TZ): Scaling server up..."
+BUILD_VERSION=$BUILD_VERSION docker compose -f $COMPOSE_FILE up -d --no-deps --scale django=2 --no-recreate django
 
-    COMPOSE_FILE="/home/matthew/democrasite/docker-compose.production.yml"
-    echo "$(date --utc +%FT%TZ): Running build..."
-    docker compose -f $COMPOSE_FILE rm -f
-    docker compose -f $COMPOSE_FILE build
+sleep 30
 
-    OLD_CONTAINER=$(docker ps -aqf "name=django")
-    echo "$(date --utc +%FT%TZ): Scaling server up..."
-    BUILD_VERSION=$BUILD_VERSION docker compose -f $COMPOSE_FILE up -d --no-deps --scale django=2 --no-recreate django
-
-    sleep 30
-
-    echo "$(date --utc +%FT%TZ): Scaling old server down..." docker container rm -f $OLD_CONTAINER
-    docker compose -f $COMPOSE_FILE up -d --no-deps --scale django=1 --no-recreate django
-elif [ $REMOTE = $BASE ]; then
-     echo "$(date --utc +%FT%TZ): Local changes detected, you may need to stash"
-else
-     echo "$(date --utc +%FT%TZ): Git is diverged, this is unexpected."
-fi
+echo "$(date --utc +%FT%TZ): Scaling old server down..."
+docker container rm -f $OLD_CONTAINER
+docker compose -f $COMPOSE_FILE up -d --no-deps --scale django=1 --no-recreate django
