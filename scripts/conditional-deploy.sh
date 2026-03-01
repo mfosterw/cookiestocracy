79b3f402a9c059364807bd1123c075420fbd927d
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2026-02-28 22:37:36 -0700

    Improve Loki (#305)
    * Add names to docker projects
    
    * Add build log to loki
    
    * Fix collector
    
    * Fix script
    
    * Fix build log
    
    * Rename grafana folder
    
    * Fix grafana
    
    * Fix script better
    
    * Improve script
    
    * Decrease loki logging
    
    * Really decrease loki logs
    
    * turn off grafana access logs
    
    * fix build log collection

--- scripts/conditional-deploy.sh
+++ scripts/conditional-deploy.sh
@@ -3,7 +3,6 @@
 # Scripts adapted from https://www.youtube.com/watch?v=jFrGhodqC08 by Tom Delande
 # Licensed under https://creativecommons.org/licenses/by/4.0/
 
-echo "$(date --utc +%FT%TZ): Fetching remote repository..."
 git fetch
 
 UPSTREAM=${1:-'@{u}'}
@@ -11,12 +10,13 @@ LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
-if [ $LOCAL = $REMOTE ]; then
-    echo "$(date --utc +%FT%TZ): No changes detected in git"
-elif [ $LOCAL = $BASE ]; then
+if [ "$LOCAL" = "$REMOTE" ]; then
+    true
+    # echo "$(date --utc +%FT%TZ): No changes detected in git"
+elif [ "$LOCAL" = "$BASE" ]; then
     git pull
     scripts/deploy.sh
-elif [ $REMOTE = $BASE ]; then
+elif [ "$REMOTE" = "$BASE" ]; then
      echo "$(date --utc +%FT%TZ): Local changes detected, you may need to stash"
 else
      echo "$(date --utc +%FT%TZ): Git is diverged, this is unexpected."
